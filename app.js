089c10c7e5af17906eb42f0f86ea119ee372f588
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-11-16 13:53:08 -0600

    home, about, error page

--- app.js
+++ app.js
@@ -1,38 +1,30 @@
-// invoke http module from builtin node
-const http = require('http')
-const { readFileSync } = require('fs');
+// here express contains the http methods/structure in 01-http-basics.js
+const express = require('express')
+const app = express()
 
-// get all files
-const homePage = readFileSync('./index2.html')
 
-// server needs to be able to receive a request from user, and send a response to user
-const server = http.createServer((req, res) => {
-    // console.log(req.method);
-    // console.log(req.url);
-    const url = req.url
-    // home page
-    if (url === '/'){
-        // writeHead() responds with success, content type is k, v pair of text/html or application/json
-        res.writeHead(200, { 'content-type': 'text/html' })
-        // if you specify content type as text/html, you can insert html elements w res.write()
-        res.write(homePage)
-        res.end()
-    }
-    // about page here
-    else if (url === '/about'){
-        res.writeHead(200, { 'content-type': 'text/html' })
-        res.write('<h1>about page</h1>')
-        res.end()
-    } 
-    // 404 error page
-    else {
-        res.writeHead(404, { 'content-type': 'text/html' })
-        res.write('<h1>page not found</h1>')
-        res.end()
-    }
+app.get('/', (req, res) => {
+    console.log('user hit the resource');
+    res.status(200).send('Home Page')
 })
 
-// server needs a port where it listens to the req, res
-// this is a communication endpoint
-// the number is arbitrary, you can use anything over 1024...but you do need it to access the application
-server.listen(5000)
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page')
+})
+
+// '*' means all paths
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>resource not found</h1>')
+})
+
+app.listen(5000, ()=>{
+    console.log('server is listening on port 5000...');
+})
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
